3c48b6cd002d215d5b8d07dcc5415e0cdcef1de8
Alexandria Thornton <kumuluvv@Mac.attlocal.net>
2025-03-15 21:37:59 -0500

    adds a nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,15 @@
 //navigation bar with links to different pages
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/calendar">Calendar</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
