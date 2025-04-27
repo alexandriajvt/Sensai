040d8a58015f1f070fb4ec7722a2fefdb61011a0
Prometheus-web-glitch <segun.olutade@gmail.com>
2025-04-27 14:50:23 -0500

    attempting to merge with segun contact page and profile page updates

---
+++ my-app/src/pages/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+import './ContactPage.css';
+
+function ContactPage() {
+  return (
+    <div className="contact-page">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default ContactPage; 
